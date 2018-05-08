c68a9ef4f7f87a092a463025e4d9abc9d23157d9
david landau <thelandaus@gmail.com>
2018-05-08 01:21:15 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,15 +3,29 @@ def instantiate_new_array
 end
 
 def array_with_two_elements
+  
+end
 
 def first_element
+  
+end
 
 def third_element
 
+end
+
 def last_element
+  
+end
 
 def first_element_with_array_methods
+  
+end
 
-last_element_with_array_methods
+def last_element_with_array_methods
+  
+end
 
-length_of_array
+def length_of_array
+  
+end
